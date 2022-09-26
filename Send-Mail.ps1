000d4c9f50588f63488b2f49b629d9b9d1a0ddaf
uzejnovicahmed <63063176+uzejnovicahmed@users.noreply.github.com>
2022-09-26 23:19:46 +0200

    Update Send-Mail.ps1

--- Send-Mail.ps1
+++ Send-Mail.ps1
@@ -1,14 +1,10 @@
 Import-Module .\Graph-Mail.psm1
 
+$TID = "913a44420-dc82-4675-aa35-837d303299ed"  #not real id 
+$CID = "99e6600c-1a3c-4600-9b44-8fe0b74333b38"  #not real id
+$CSECRET = "adfgasdgaerzhhadfgadffgdsagdfhdfhgfdh" #dumysecret
 
-#$TID = "913a44420-dc82-4675-aa35-837d303299ed"  #not real id 
-#$CID = "99e6600c-1a3c-4600-9b44-8fe0b74333b38"  #not real id
-#$CSECRET = "adfgasdgaerzhhadfgadffgdsagdfhdfhgfdh" #dumysecret
-$TID = "913a0020-dc82-4a35-aa35-837d303299ed"
-$CID = "99e6600c-1a3c-4100-9b44-8fe0b7893b38"
-$CSECRET = ".GT8Q~tclExvauVnDuS7dMW1gr~~.YZe-68j7dtq"
-
-Initializing-GraphMail -TENANTID $TID -CLIENTID $CID -CLIENTSECRET $CSECRET -MAILSENDER "AdeleV@uzeah.onmicrosoft.com" -MAILRECIPIENT "ahmed.uzejnovic@baseit.at"
+Initializing-GraphMail -TENANTID $TID -CLIENTID $CID -CLIENTSECRET $CSECRET -MAILSENDER "AdeleV@uzeah.onmicrosoft.com" -MAILRECIPIENT "ahmeduzejnovic@hotmail.com"
 
 
 Send-GraphMail -HTML_CONTENT $data -Subject "Testsubject"
