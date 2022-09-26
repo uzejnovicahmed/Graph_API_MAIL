5a80f523bc79c7628726ba59237b852b815d20e6
Uzejnovic Ahmed <63063176+uzejnovicahmed@users.noreply.github.com>
2022-09-27 01:06:20 +0200

    Update and rename Send-Mail.ps1 to Send-Mail-Demo.ps1

--- Send-Mail-Demo.ps1
+++ Send-Mail-Demo.ps1
@@ -4,8 +4,9 @@ $TID = "913a44420-dc82-4675-aa35-837d303299ed"  #not real id
 $CID = "99e6600c-1a3c-4600-9b44-8fe0b74333b38"  #not real id
 $CSECRET = "adfgasdgaerzhhadfgadffgdsagdfhdfhgfdh" #dumysecret
 
-Initializing-GraphMail -TENANTID $TID -CLIENTID $CID -CLIENTSECRET $CSECRET -MAILSENDER "AdeleV@uzeah.onmicrosoft.com" -MAILRECIPIENT "ahmeduzejnovic@hotmail.com"
+Request-GraphMail -TENANTID $TID -CLIENTID $CID -CLIENTSECRET $CSECRET -MAILSENDER "AdeleV@dev.onmicrosoft.com" -MAILRECIPIENT "dev.test@ms.com"
 
+$data = "test"
 
 Send-GraphMail -HTML_CONTENT $data -Subject "Testsubject"
 
